9b8ab87afeb9a0db79a745779e7eb0b80a99298a
sfernandezrod <sfernandezrod@indra.es>
2024-09-30 17:23:32 +0200

    Commit Tarea 1

---
+++ Tareas/src/Tarea1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tarea1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingrese su edad: ");
+        String edadString = scanner.nextLine();
+        int edad = Integer.parseInt(edadString);
+
+        System.out.println("Ingrese su ciudad de origen: ");
+        String ciudad = scanner.nextLine();
+
+        System.out.println("Ingrese una frase personalizada: ");
+        String frasePersonalizada = scanner.nextLine();
+
+        String fraseCompleta = "Hola, mi nombre es " + nombre + ", tengo " + edad + " años, soy de " + ciudad + " y " + frasePersonalizada;
+
+        System.out.println(fraseCompleta);
+    }
+}
+
